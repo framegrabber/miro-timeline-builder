7fbb9ab89a2fa2d6bf3dd2173e6888dba3c6faac
Felix Rothballer <felix.rothballer@gmail.com>
2024-02-02 19:33:58 +0100

    removed quarters plugin

--- browser-inspector-import.js
+++ browser-inspector-import.js
@@ -1,13 +1,11 @@
 await import("https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js")
 import("https://cdn.jsdelivr.net/npm/dayjs@1/plugin/isoWeeksInYear.js")
-window.dayjs.extend(window.dayjs_plugin_isoWeeksInYear)
-import("https://cdn.jsdelivr.net/npm/dayjs@1/plugin/isLeapYear.js")
-window.dayjs.extend(window.dayjs_plugin_isLeapYear)
-import("https://cdn.jsdelivr.net/npm/dayjs@1/plugin/quarterOfYear.js")
-window.dayjs.extend(window.dayjs_plugin_quarterOfYear)
-import("https://cdn.jsdelivr.net/npm/dayjs@1/plugin/isoWeek.js")
-window.dayjs.extend(window.dayjs_plugin_isoWeek)
 import("https://cdn.jsdelivr.net/npm/dayjs@1/plugin/weekday.js")
+import("https://cdn.jsdelivr.net/npm/dayjs@1/plugin/isLeapYear.js")
+import("https://cdn.jsdelivr.net/npm/dayjs@1/plugin/isoWeek.js")
+window.dayjs.extend(window.dayjs_plugin_isoWeeksInYear)
+window.dayjs.extend(window.dayjs_plugin_isLeapYear)
+window.dayjs.extend(window.dayjs_plugin_isoWeek)
 window.dayjs.extend(window.dayjs_plugin_weekday)
 
 
